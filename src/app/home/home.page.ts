b98cd729893e480c06b17ccae03ee8312ca5188e
2010jmatpon <jmatpon2010@g.educaand.es>
2023-10-05 11:18:04 +0200

    commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -7,6 +7,39 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  numSecreto: number = 0;
+  numUsuario: number = 0;
+  txtMensaje: string = "";
+  esCorrecto: boolean= false;
+  
+  constructor() {
+    this.generarNumSecreto();
+  }
+  
+  reiniciar(){
+    this.generarNumSecreto();
+    this.esCorrecto=false;
+    this.numUsuario=0;
+    this.txtMensaje="";
+  }
+
+  comprobarNumero(){
+    console.log("click en botón");
+    console.log("numUsuario="+this.numUsuario);
+    if(this.numUsuario == this.numSecreto){
+      this.txtMensaje = "Has acertado";
+      this.esCorrecto=true;
+    } else if (this.numSecreto < this.numUsuario){
+      this.txtMensaje="El Número Secreto es menor";
+    } else{
+      this.txtMensaje="El Número Secreto es mayor";
+    }
+  }
+  generarNumSecreto(){
+    this.numSecreto=Math.floor(Math.random()*100+1);
+    console.log("numSecreto="+this.numSecreto);
+  }
+
+
 
 }
